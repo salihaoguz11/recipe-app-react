ca2d84d652a402c054fa97b0d654b2017ba28670
Saliha <salihaoguz2011@gmail.com>
2023-03-06 20:13:34 +0000

    navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,18 +1,23 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Brand, Menu, MenuLink, Navbar } from "./Nav.style";
+
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div>
-      <Link to="/">
+    <Navbar justify="space-between">
+      <Brand to="/">
         <i>{"Saliha"}</i>
         <span>Recipe</span>
-      </Link>
+      </Brand>
 
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="register">Register</Link>
-        <Link to="logout">Logout</Link>
-      </ul>
-    </div>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="logout">Logout</MenuLink>
+      </Menu>
+    </Navbar>
   );
 };
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,10 +3,14 @@ import Detail from "../pages/detail/Detail";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Nav from "../components/nav/Nav";
+import { GlobalStyle } from "../components/globalStyles/GlobalStyle";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
