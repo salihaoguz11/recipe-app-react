7704d4a120ea077dc2fc268f6283a63da497c7a0
Saliha <salihaoguz2011@gmail.com>
2023-03-07 08:31:18 +0000

    login

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+
 import { Brand, Hamburger, Menu, MenuLink, Navbar } from "./Nav.style";
 import { GiHamburgerMenu } from "react-icons/gi";
 
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,20 @@
+import meal from "../../assets/meal.svg";
+import { FormContainer, LoginContainer } from "./Login.style";
+
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <img src={meal} alt="" width={250} />
+        <h1> Recipe</h1>
+        <form action="">
+          <input type="text" placeholder="Enter username" />
+          <input type="text" placeholder="Enter password" />
+          <button type="submit">LOGIN</button>
+        </form>
+      </FormContainer>
+    </LoginContainer>
+  );
 };
 
 export default Login;
